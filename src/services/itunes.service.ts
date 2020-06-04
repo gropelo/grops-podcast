062ca9b4c1515b5c397d1bafef2c2b83b8241173
Danilo Gropelo <dgropelo@gmail.com>
2020-06-04 14:38:47 -0300

    Using cors proxy

--- src/services/itunes.service.ts
+++ src/services/itunes.service.ts
@@ -2,5 +2,5 @@ import axios, { AxiosResponse } from 'axios';
 import { ISearchResult } from '../types/itunes.types';
 
 export async function search(query: string): Promise<AxiosResponse<ISearchResult>> {
-  return axios.get<ISearchResult>(`http://itunes.apple.com/search?term=${query}&media=podcast`);
+  return axios.get<ISearchResult>(`https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=${query}&media=podcast`);
 }
