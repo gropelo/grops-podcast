7cf4c73406c2d4c197a789d8a765757b77b3dbc0
Danilo Gropelo <dgropelo@gmail.com>
2020-06-17 12:51:56 -0300

    Throttling api call.

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -2,26 +2,35 @@ import { useState, useEffect } from 'react';
 import { search } from '../services/app.service';
 import { Status, IPodcasts } from '../types/app.types';
 import Axios from 'axios';
+import { throttle } from 'throttle-debounce';
+
+const throttling = throttle(500, (callback) => {
+  callback();
+}, false);
 
 export function useSearch(query: string): [IPodcasts, Status] {
   const [results, setResults] = useState<IPodcasts>([]);
   const [status, setStatus] = useState(Status.NONE);
 
+
   useEffect(() => {
     if (!query || query.trim().length < 3) return;
 
     setStatus(Status.LOADING);
-    search(query)
-      .then(podcasts => {
-        setResults(podcasts);
-        setStatus(Status.READY);
-      }).catch(err => {
-        if (Axios.isCancel(err)) {
-          setStatus(Status.LOADING);
-        } else {
-          setStatus(Status.ERROR);
-        }
-      });
+
+    throttling(() => {
+      search(query)
+        .then(podcasts => {
+          setResults(podcasts);
+          setStatus(Status.READY);
+        }).catch(err => {
+          if (Axios.isCancel(err)) {
+            setStatus(Status.LOADING);
+          } else {
+            setStatus(Status.ERROR);
+          }
+        });
+    });
   }, [query]);
 
   return [results, status];
